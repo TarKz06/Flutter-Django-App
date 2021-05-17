d57c888b7dc532ccfda2fd65da3b0195795e9902
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-05-17 14:39:04 +0700

    add sukuna and calulate shop

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,112 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.pink
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('First app by tar'),
+              centerTitle: true,
+            ),
+            body: Home()));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomeState extends State<Home> {
+  var ctl_product = TextEditingController();
+  var ctl_price = TextEditingController();
+  var ctl_quantity = TextEditingController();
+  var result = '------- result ------------';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return ListView(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Center(
+              child: Text(
+                'TarKz-Soft',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                    color: Colors.blue[900],
+                    fontFamily: 'sd'),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Icon(
+              Icons.alarm,
+              size: 100.0,
+              color: Colors.blue[900],
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Padding(
+          padding: EdgeInsets.fromLTRB(50, 25, 50, 10),
+          child: Image.asset(
+            'assets/sukuna.jpg',
+            scale: 0.7,
+          ),
+        ),
+        Padding(
+            padding: EdgeInsets.fromLTRB(50, 25, 50, 10),
+            child: TextField(
+              controller: ctl_product,
+              decoration: InputDecoration(
+                  labelText: 'Product', border: OutlineInputBorder()),
+            )),
+        Padding(
+            padding: EdgeInsets.fromLTRB(50, 25, 50, 10),
+            child: TextField(
+              controller: ctl_price,
+              decoration: InputDecoration(
+                  labelText: 'Pre Nmber', border: OutlineInputBorder()),
+            )),
+        Padding(
+            padding: EdgeInsets.fromLTRB(50, 25, 50, 10),
+            child: TextField(
+              controller: ctl_quantity,
+              decoration: InputDecoration(
+                  labelText: 'Price', border: OutlineInputBorder()),
+            )),
+        Padding(
+              padding: EdgeInsets.fromLTRB(50, 25, 50, 10),
+            child: ElevatedButtonTheme(
+                child:ElevatedButton(
+                onPressed: () {
+                  var v1 = int.parse(ctl_price.text);
+                  var v2 = int.parse(ctl_quantity.text);
+                  var cal = v1 * v2;
+                  print("cal :$cal Bath ");
+                  var productname = ctl_product.text;
+                  setState(() {
+                    result =
+                        "have product : $productname\nprice:$v1\n number: $v2 \n Total : $cal";
+                  });
+                },
+                child: Row(
+                    children: [Icon(Icons.send), 
+                    Text('massage to user')]
+                    )
+                  )
+                ),
+        ),
+        Padding(
+            padding: EdgeInsets.fromLTRB(50, 40, 50, 10),
+            child: Text(
+              result,
+              style: TextStyle(fontSize: 30.0),
+            )),
+      ],
     );
   }
 }
